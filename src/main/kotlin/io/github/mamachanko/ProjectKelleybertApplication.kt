dad209f2e0b8450a1330e55f5149ed494a9903a0
Max Brauer <mbrauer@pivotal.io>
2017-01-19 12:45:09 +0100

    initial commit

---
+++ src/main/kotlin/io/github/mamachanko/ProjectKelleybertApplication.kt
@@ -0,0 +1,11 @@
+package io.github.mamachanko
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ProjectKelleybertApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ProjectKelleybertApplication::class.java, *args)
+}
---
+++ src/test/kotlin/io/github/mamachanko/ProjectKelleybertApplicationTests.kt
@@ -0,0 +1,16 @@
+package io.github.mamachanko
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ProjectKelleybertApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
