171b58562cd56ca1d9dd92d92a586f36795f9fee
mamachanko <sugardubz@gmail.com>
2017-02-05 14:35:13 +0100

    randomly pick color and strategy

--- api/src/main/kotlin/io/github/mamachanko/DrawingService.kt
+++ api/src/main/kotlin/io/github/mamachanko/DrawingService.kt
@@ -1,18 +1,18 @@
 package io.github.mamachanko
 
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class DrawingService(val pageTemplates: Set<PageTemplate>, val colorPalettes: Set<Palette>, val strategies: Set<Strategy>) {
 
     fun getDrawing(width: Double, height: Double): Drawing {
-//        TODO: randomly pick palette and strategy
         val pageTemplate = pageTemplates.first()
         val page = Page(width = width, height = height, layout = pageTemplate.layout, grid = pageTemplate.grid)
         return Drawing(
                 page = page,
-                palette = colorPalettes.first(),
-                strategy = strategies.first()
+                palette = colorPalettes.toList()[Random().nextInt(colorPalettes.size)],
+                strategy = strategies.toList()[Random().nextInt(strategies.size)]
         )
     }
 }
