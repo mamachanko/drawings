db07a511a546e6ed61e1fbd0c87af4ee807364b4
dilabuser <dilabuser@havoc-02.local>
2017-02-06 12:13:20 +0100

    continue to work on rewriting

--- api/src/test/kotlin/io/github/mamachanko/InstructionsTests.kt
+++ api/src/test/kotlin/io/github/mamachanko/InstructionsTests.kt
@@ -10,9 +10,15 @@ class InstructionsTests {
         val instructions = ToAPage()
                 .withWidth(500.0).and().withHeight(900.0)
                 .add().a().rectangle().fillingThePage()
+                .then()
+                .add().two().rectangles().fillingThePage()
+                .then()
+                .discard().one()
+                .then()
+                .duplicate().all()
                 .andSee()
 
-        assertThat(instructions.execute().shapes).hasSize(0)  // TODO: this is wrong. continue here.
+        assertThat(instructions.execute().shapes).hasSize(4)
     }
 
 }
@@ -33,15 +39,113 @@ class ToAPage(var width: Double = .0, var height: Double = .0) {
         return this
     }
 
-    fun add(): Instructions {
-        val precedingInstructions = Instructions(initial = this, instructions = emptyList<Instruction>())
-        return Add(precedingInstructions = precedingInstructions)
+    fun add(): Add {
+        val emptyDrawing = ADrawing()
+        val precedingInstructions = Instructions(emptyDrawing, emptyList<Instruction>())
+        return Add(precedingInstructions)
     }
 
 }
 
-class Instructions(initial: ToAPage, instructions: List<Instruction>)
+class Instructions(val initial: ADrawing, val instructions: List<Instruction>) {
 
-abstract class Instruction(val precedingInstructions = Instructions) {
+    fun plus(newInstruction: Instruction): Instructions {
+        return Instructions(initial, instructions.plus(newInstruction))
+    }
 
+    fun execute(): ADrawing {
+        return instructions.fold(initial, {state, instruction -> instruction.applyTo(state)})
+    }
+
+    fun add(): Add {
+        return Add(this)
+    }
+
+    fun discard(): Discard {
+        return Discard(this)
+    }
+
+    fun duplicate(): Duplicate {
+        return Duplicate(this)
+    }
+}
+
+abstract class Instruction(val precedingInstructions: Instructions) {
+
+    fun andSee(): Instructions {
+        return then()
+    }
+
+    fun then(): Instructions {
+        return precedingInstructions.plus(this)
+    }
+
+    abstract fun applyTo(state: ADrawing): ADrawing
+
+}
+class ADrawing(val shapes: List<AShape> = emptyList()) {
+
+    fun plusShapes(shapes: List<AShape>): ADrawing {
+        return ADrawing(this.shapes.plus(shapes))
+    }
+
+}
+
+class AShape
+
+class Add(precedingInstructions: Instructions) : Instruction(precedingInstructions) {
+
+    override fun applyTo(state: ADrawing): ADrawing {
+        if (count == 1) {
+            return state.plusShapes(listOf(AShape()))
+        }
+        return state.plusShapes(listOf(AShape(), AShape()))
+    }
+
+    private var count: Int = 0
+
+    fun a(): Add {
+        count = 1
+        return this
+    }
+
+    fun two(): Add {
+        count = 2
+        return this
+    }
+
+    fun rectangle(): Add {
+        return this
+    }
+
+    fun fillingThePage(): Add {
+        return this
+    }
+
+    fun rectangles(): Add {
+        return this
+    }
+
+}
+
+class Discard(precedingInstructions: Instructions) : Instruction(precedingInstructions) {
+    override fun applyTo(state: ADrawing): ADrawing {
+        return ADrawing(state.shapes.drop(1))
+    }
+
+    fun one(): Discard {
+        return this
+    }
+
+}
+
+
+class Duplicate(precedingInstructions: Instructions) : Instruction(precedingInstructions) {
+    override fun applyTo(state: ADrawing): ADrawing {
+        return state.plusShapes(state.shapes)
+    }
+
+    fun all(): Duplicate {
+        return this
+    }
 }
