aed5cb1638f5fd4a524d4f7bae85a01c3d8047f8
dilabuser <dilabuser@havoc-02.local>
2017-02-07 12:48:25 +0100

    work on colorise

--- api/src/test/kotlin/io/github/mamachanko/InstructionsTests.kt
+++ api/src/test/kotlin/io/github/mamachanko/InstructionsTests.kt
@@ -15,6 +15,8 @@ class InstructionsTests {
                 .discard().one()
                 .then()
                 .duplicate().all()
+                .then()
+                .colorise().half().from(GrayPalette())
 
         val drawing = GivenABlank()
                 .withWidth(600.0).and().withHeight(800.0)
@@ -25,6 +27,8 @@ class InstructionsTests {
         drawing.shapes.map {
             assertThat(it.vertices).containsExactly(Vertex2(.0, .0), Vertex2(600.0, .0), Vertex2(600.0, 800.0), Vertex2(.0, 800.0))
         }
+        assertThat(drawing.shapes.filter { !it.color.equals(SOLID_BLACK) }).hasSize(2)
+        assertThat(drawing.shapes.filter { it.color.equals(SOLID_BLACK) }).hasSize(2)
     }
 
 }
@@ -37,13 +41,10 @@ fun GivenABlank(): Drawing2 {
     return Drawing2()
 }
 
-class Drawing2(val shapes: List<Shape2> = emptyList()) {
-
-    var width: Double = .0
-    var height: Double = .0
+data class Drawing2(val width: Double = .0, val height: Double = .0, val shapes: List<Shape2> = emptyList()) {
 
     fun plusShapes(shapes: List<Shape2>): Drawing2 {
-        return Drawing2(this.shapes.plus(shapes))
+        return this.copy(shapes = this.shapes.plus(shapes))
     }
 
     fun follow(instructions: List<Instruction>): Drawing2 {
@@ -51,13 +52,15 @@ class Drawing2(val shapes: List<Shape2> = emptyList()) {
     }
 
     fun withWidth(width: Double): Drawing2 {
-        this.width = width
-        return this
+        return this.copy(width = width)
     }
 
     fun withHeight(height: Double): Drawing2 {
-        this.height = height
-        return this
+        return this.copy(height = height)
+    }
+
+    fun withShapes(shapes: List<Shape2>): Drawing2 {
+        return this.copy(shapes = shapes)
     }
 
     fun and(): Drawing2 {
@@ -66,9 +69,14 @@ class Drawing2(val shapes: List<Shape2> = emptyList()) {
 
 }
 
-data class Shape2(val vertices: List<Vertex2> = emptyList()) {
+data class Shape2(val vertices: List<Vertex2> = emptyList(), val color: Color = SOLID_BLACK) {
+
     fun withVertices(vararg vertices: Vertex2): Shape2 {
-        return Shape2(vertices.asList())
+        return this.copy(vertices = vertices.asList())
+    }
+
+    fun withColour(color: Color): Shape2 {
+        return this.copy(color = color)
     }
 }
 
@@ -86,6 +94,10 @@ abstract class Instruction(val prior: List<Instruction> = emptyList()) {
 
     fun duplicate(): Duplicate {
         return Duplicate(prior.plus(this))
+    }
+
+    fun colorise(): Colorise {
+        return Colorise(prior.plus(this))
     }
 
     fun then(): Instruction {
@@ -142,7 +154,7 @@ class Add(prior: List<Instruction> = emptyList()) : Instruction(prior) {
 class Discard(prior: List<Instruction> = emptyList()) : Instruction(prior) {
 
     override fun applyTo(state: Drawing2): Drawing2 {
-        return Drawing2(state.shapes.drop(1))
+        return state.withShapes(state.shapes.drop(1))
     }
 
     fun one(): Discard {
@@ -152,11 +164,36 @@ class Discard(prior: List<Instruction> = emptyList()) : Instruction(prior) {
 }
 
 class Duplicate(prior: List<Instruction> = emptyList()) : Instruction(prior) {
+
     override fun applyTo(state: Drawing2): Drawing2 {
         return state.plusShapes(state.shapes)
     }
 
     fun all(): Duplicate {
+        return this
+    }
+
+}
+
+class Colorise(prior: List<Instruction>) : Instruction(prior) {
+
+    private var palette: Palette = BlackPalette()
+
+    override fun applyTo(state: Drawing2): Drawing2 {
+        val shapes = state.shapes.map { shape -> shape.withColour(palette.color) }
+        return state.withShapes(shapes)
+    }
+
+    fun all(): Colorise {
+        return this
+    }
+
+    fun from(palette: Palette): Colorise {
+        this.palette = palette
+        return this
+    }
+
+    fun half(): Colorise {
         return this
     }
 
