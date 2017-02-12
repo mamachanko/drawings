1d008b499fd36172eb22c25622903c655bca2957
mamachanko <sugardubz@gmail.com>
2017-02-12 21:23:28 +0100

    add more palettes and instructions

--- api/src/main/kotlin/io/github/mamachanko/config/Instructions.kt
+++ api/src/main/kotlin/io/github/mamachanko/config/Instructions.kt
@@ -13,12 +13,37 @@ class Instructions {
     @Bean
     fun baseline(): List<Instruction> {
         return StartBy()
-                .adding().rectangles().to(aGridOf(2 x 3).withCollapsedMargin(10.0))
+                .adding().rectangles().to(aGridOf(2 x 3).withCollapsedMargin(2.0))
                 .then()
                 .duplicate().all()
                 .then()
                 .shave().all().randomly()
                 .then()
+                .colorise().all()
+                .asList()
+    }
+
+    @Bean
+    fun riley(): List<Instruction> {
+        return StartBy()
+                .adding().rectangles().to(aGridOf(8 x 1).withCollapsedMargin(2.0))
+                .colorise().all()
+                .asList()
+    }
+
+    @Bean
+    fun rothko(): List<Instruction> {
+        return StartBy()
+                .adding().rectangles().to(aGridOf(1 x 3).withCollapsedMargin(2.0))
+                .colorise().all()
+                .asList()
+    }
+
+    @Bean
+    fun pixels(): List<Instruction> {
+        return StartBy()
+                .adding().rectangles().to(aGridOf(15 x 20).withCollapsedMargin(.0))
+                .shave().all()
                 .colorise().all()
                 .asList()
     }
