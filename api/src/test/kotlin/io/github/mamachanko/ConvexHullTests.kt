6fe610b292602d3f5963b93fac424ee3c982b82b
dilabuser <dilabuser@havoc-5.local>
2017-02-17 12:42:32 +0100

    continue to work on convex hull

--- api/src/test/kotlin/io/github/mamachanko/ConvexHullTests.kt
+++ api/src/test/kotlin/io/github/mamachanko/ConvexHullTests.kt
@@ -27,19 +27,11 @@ class OrientationTest {
         assertThat(orienationOf(Vertex(1.0, 1.0), Vertex(3.0, 2.0), Vertex(5.0, 3.0))).isEqualTo(Orientation.COLINEAR)
     }
 
-    private fun orienationOf(a: Vertex, b: Vertex, c: Vertex): Orientation {
-        val orientation = (b.y - a.y) * (c.x - b.x) - (b.x - a.x) * (c.y - b.y)
-        return when {
-            orientation < 0 -> Orientation.CLOCKWISE
-            orientation > 0 -> Orientation.COUNTERCLOCKWISE
-            else -> Orientation.COLINEAR
-        }
-    }
 }
 
 class ConvexHullTests {
 
-//    @Test
+    @Test
     fun `should return convex hull of a set of vertices`() {
         assertThat(convexHull(setOf(
                 Vertex(1.0, 1.0),
@@ -60,7 +52,7 @@ class ConvexHullTests {
         )
     }
 
-//    @Test
+    @Test
     fun `should return convex hull of a triangle as the triangle itself`() {
         assertThat(convexHull(setOf(
                 Vertex(1.0, 1.0),
@@ -73,7 +65,7 @@ class ConvexHullTests {
         )
     }
 
-//    @Test
+    @Test
     fun `should return convex hull of a convex quadriliteral as the quadriliteral itself`() {
         assertThat(convexHull(setOf(
                 Vertex(1.0, 1.0),
@@ -88,7 +80,7 @@ class ConvexHullTests {
         )
     }
 
-//    @Test
+    @Test
     fun `should return convex hull of a concave quadriliteral as its convex triangle`() {
         assertThat(convexHull(setOf(
                 Vertex(3.0, 0.0),
@@ -104,17 +96,44 @@ class ConvexHullTests {
 }
 
 fun convexHull(vertices: Set<Vertex>): Set<Vertex> {
-    return setOf(leftMost(vertices), rightMost(vertices))
+    val start = leftMost(vertices)
+    val convexHull = mutableSetOf(start)
+    var next = start
+    while (true) {
+        next = findNextOnConvexHull(vertices.minus(next), next)
+        if (next == start) {
+            break
+        } else {
+            convexHull.add(next)
+        }
+    }
+    return convexHull
 }
 
-private fun leftMost(vertices: Set<Vertex>): Vertex {
+fun findNextOnConvexHull(vertices: Set<Vertex>, current: Vertex): Vertex {
+    println("finding next vertex after $current")
+    val next = vertices.find {
+        println("  looking at $it")
+        vertices.minus(it).filter { x ->
+            println("    comparing with $x")
+            val orienationOf = orienationOf(current, it, x)
+            println("      the orientationOf($current, $it, $x) is $orienationOf")
+            orienationOf == Orientation.CLOCKWISE
+        }.size == vertices.size - 1
+    }
+
+    return next!!
+}
+
+fun leftMost(vertices: Set<Vertex>): Vertex {
     return vertices.sortedWith(compareBy { it.x }).first()
 }
 
-private fun rightMost(vertices: Set<Vertex>): Vertex {
-    return vertices.sortedWith(compareBy { it.x }).last()
-}
-
-private fun Edge.isLeftOf(vertex: Vertex): Boolean {
-    return (vertex.x - a.x) * (b.y - a.y) - (vertex.y - a.y) * (b.x - a.x) < 0
+fun orienationOf(a: Vertex, b: Vertex, c: Vertex): Orientation {
+    val orientation = (b.y - a.y) * (c.x - b.x) - (b.x - a.x) * (c.y - b.y)
+    return when {
+        orientation < 0 -> Orientation.CLOCKWISE
+        orientation > 0 -> Orientation.COUNTERCLOCKWISE
+        else -> Orientation.COLINEAR
+    }
 }
