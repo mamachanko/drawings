55a69bfc715188fdc97bb252f1673e749aa2a268
dilabuser <dilabuser@havoc-5.local>
2017-02-28 08:23:01 +0100

    improve Perlin noise representation

--- sketches/sketches/sketch.js
+++ sketches/sketches/sketch.js
@@ -15,7 +15,8 @@ const cellHeight = height / rows;
 
 var xoff = 0;
 var yoff = 0;
-const offInc = .02;
+const xoffInc = .5;
+const yoffInc = .04;
 
 function setup() {
     createCanvas(totalWidth, totalHeight);
@@ -24,26 +25,21 @@ function setup() {
 
 function draw() {
     clear();
-    rect(originX, originY, width, height);
+    // rect(originX, originY, width, height);
     fill(1);
 
     for (y = 0; y < rows; y++) {
         for (x = 0; x < cols; x++) {
 
-            // rect(originX + (x * cellWidth), originY + (y * cellHeight), cellWidth, cellHeight);
-            // point(originX + (x * cellWidth) + cellWidth * .5, originY + (y * cellHeight) + cellHeight * .5);
-            const radius = map(noise(xoff, yoff), 0, 1, 1, cellWidth);
-            // const random = map(random(1), 0, 1, - (cellWidth * 5), (cellWidth * 5));
+            const radius = map(noise(xoff, yoff), 0, 1, 0, cellWidth * 1.75);
             ellipse(
                 originX + (x * cellWidth) + cellWidth * .5,
                 originY + (y * cellHeight) + cellHeight * .5,
                 radius,
                 radius
             );
-            // xoff += offInc * (width / height);
-            xoff += .15;
+            xoff += xoffInc;
         }
-        // yoff += offInc;
-        yoff += .01;
+        yoff += yoffInc;
     }
 }
