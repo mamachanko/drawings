6d4606a05b4634d0af982fd6d456dad0b409b4bb
dilabuser <dilabuser@havoc-5.local>
2017-03-02 12:44:17 +0100

    sketch with motion

--- sketches/src/index.js
+++ sketches/src/index.js
@@ -4,6 +4,7 @@ import _01 from './01';
 import _02 from './02';
 import _03 from './03';
 import _04 from './04';
+import _05 from './05';
 
 const router = new Navigo();
 
@@ -12,7 +13,8 @@ router.on({
     '02': () => { _02() },
     '03': () => { _03() },
     '04': () => { _04() },
+    '05': () => { _05() },
     '*': () => {
         let app = document.querySelector('#app');
-        app.innerHTML = '<ul><li><a href="01">01</a></li><li><a href="02">02</a></li><li><a href="03">03</a></li><li><a href="04">04</a></li></ul>';
+        app.innerHTML = '<ul><li><a href="01">01</a></li><li><a href="02">02</a></li><li><a href="03">03</a></li><li><a href="04">04</a></li><li><a href="05">05</a></li></ul>';
     }}).resolve();
