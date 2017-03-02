e2e28d4cfcf85694040ebaf9708f826e7adc3c00
dilabuser <dilabuser@havoc-5.local>
2017-03-02 12:29:19 +0100

    another sketch

--- sketches/src/index.js
+++ sketches/src/index.js
@@ -3,6 +3,7 @@ import Navigo from 'navigo';
 import _01 from './01';
 import _02 from './02';
 import _03 from './03';
+import _04 from './04';
 
 const router = new Navigo();
 
@@ -10,7 +11,8 @@ router.on({
     '01': () => { _01() },
     '02': () => { _02() },
     '03': () => { _03() },
+    '04': () => { _04() },
     '*': () => {
         let app = document.querySelector('#app');
-        app.innerHTML = '<ul><li><a href="01">01</a></li><li><a href="02">02</a></li><li><a href="03">03</a></li></ul>';
+        app.innerHTML = '<ul><li><a href="01">01</a></li><li><a href="02">02</a></li><li><a href="03">03</a></li><li><a href="04">04</a></li></ul>';
     }}).resolve();
