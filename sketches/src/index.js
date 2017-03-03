b5514e0d50b3f349da1796c1dd28fb0f00a9a4ed
mamachanko <sugardubz@gmail.com>
2017-03-03 21:29:14 +0100

    import all sketches. serve them randomly. automatically generate routes.

--- sketches/src/index.js
+++ sketches/src/index.js
@@ -1,22 +1,28 @@
 import './index.css';
-
+import * as sketches from './sketches';
 import Navigo from 'navigo';
 
-import _01 from './01';
-import _02 from './02';
-import _03 from './03';
-import _04 from './04';
-import _05 from './05';
+let routes = {};
+Object.values(sketches)
+    .map((sketch, i) =>
+        routes[`0${i + 1}`] = () => sketch()
+    );
 
-const router = new Navigo();
+routes['any'] = () => {
+    let sketchesArray = Object.values(sketches);
+    sketchesArray[Math.floor(Math.random() * sketchesArray.length)]();
+};
 
-router.on({
-    '01': () => { _01() },
-    '02': () => { _02() },
-    '03': () => { _03() },
-    '04': () => { _04() },
-    '05': () => { _05() },
-    '*': () => {
-        let app = document.querySelector('#app');
-        app.innerHTML = '<ul><li><a href="01">01</a></li><li><a href="02">02</a></li><li><a href="03">03</a></li><li><a href="04">04</a></li><li><a href="05">05</a></li></ul>';
-    }}).resolve();
+routes['*'] = () => {
+    let app = document.querySelector('#app');
+    app.innerHTML = '<ul>' +
+        '<li><a href="01">01</a></li>' +
+        '<li><a href="02">02</a></li>' +
+        '<li><a href="03">03</a></li>' +
+        '<li><a href="04">04</a></li>' +
+        '<li><a href="05">05</a></li>' +
+        '<li><a href="any">any</a></li>' +
+        '</ul>';
+};
+
+new Navigo().on(routes).resolve();
